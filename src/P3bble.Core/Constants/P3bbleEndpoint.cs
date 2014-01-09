99bc12f98dd776722a0a2bc463b2a5eb91a16ce3
Steve Robbins <steve.robbins@nokia.com>
2014-01-09 16:41:04 +0000

    Updated endpoint list to match latest set

--- src/P3bble.Core/Constants/P3bbleEndpoint.cs
+++ src/P3bble.Core/Constants/P3bbleEndpoint.cs
@@ -18,16 +18,19 @@ namespace P3bble.Core.Constants
         SystemMessage = 18,
         MusicControl = 32,
         PhoneControl = 33,
+        ApplicationMessage = 48,
+        Launcher = 49,
         Logs = 2000,
         Ping = 2001,
         LogsDump = 2002,
         Reset = 2003,
-        AppMfg = 2004,
+        //AppMfg = 2004, - seems unused
+        AppLogs = 2006,
         Notification = 3000,
         Resource = 4000,
         SysReg = 5000,
         FctReg = 5001,
-        AppInstallManager = 6000,
+        AppManager = 6000,
         RunKeeper = 7000,
         PutBytes = 48879
     }
